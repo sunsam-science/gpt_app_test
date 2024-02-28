92e8dc1c897d13270c2d3854cca567fe18745290
sunsam-science <82987776+sunsam-science@users.noreply.github.com>
2024-02-28 23:32:41 +0900

    Update app.py
    support korean now

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ client = OpenAI(
     api_key = st.secrets["API_KEY"]
 )
 
-st.title("개떡같이 말해도 알아서 잘 그려주는 사이트")
+st.title("그려달라는거 대충말하면 대충 그려주는 사이트 ver1.1.")
 
 with st.form("form"):
-    user_input = st.text_input("당신이 생성하고 싶은 이미지를 개떡같더라도 영어로 적어보세요")
-    submit = st.form_submit_button("호옹이!")
+    user_input = st.text_input("당신이 생성하고 싶은 이미지를 대충 적어ㅗㅅㅔ요")
+    submit = st.form_submit_button("옛다!")
 
 if submit and user_input:
     # st.write(user_input)
     gpt_prompt = [{
         "role" : "system",
-        "content" : "Imagine the detail appeareance of the input. Response it shortly around 20 words."
+        "content" : "If Korean, translate to English; if English, proceed. Briefly imagine detailed appearance. Respond concisely in around 20 words."
     }]
 
     gpt_prompt.append({
